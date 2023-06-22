f93fff8cecaf5b77bcfdfc588445608e3dd9f992
Souheila <souheila.sidennas@gmail.com>
2023-06-22 23:13:13 +0100

    updates in main and Readme

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-<>
+# 0x19. C - Stacks, Queues - LIFO, FIFO
+![](https://pbs.twimg.com/media/CFYYWy6UEAE9Ow-.png)
+
+### Authors
+[Zakaria Khonchafi](https://www.github.com/zakaria-alx) 
+[Souheila Sidennas](https://www.github.com/Souheilaso)
--- main.c
+++ main.c
@@ -4,11 +4,11 @@
 #define MAX_LINE_LENGTH 1024
 
 bus_t bus = {NULL, NULL, NULL, 0};
-
 /**
-  * main - entry point of monty program
-  * argc: arg count
-  * argv: arg list
+* main - monty code interpreter
+* @argc: number of arguments
+* @argv: monty file location
+* Return: 0 on success
 */
 int main(int argc, char *argv[])
 {
